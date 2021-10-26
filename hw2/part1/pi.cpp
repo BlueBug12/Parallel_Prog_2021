593c5f856772b956d898a60e652d4e0ffdb8e8c2
BlueBug12 <benson.liu.himself@gmail.com>
2021-10-26 14:48:06 +0000

    faster version(mutex)

--- hw2/part1/pi.cpp
+++ hw2/part1/pi.cpp
@@ -9,22 +9,30 @@ typedef struct
    int iteration;
 } Arg;
 
-long long int circle[16];
+//long long int circle[16];
+long long int total_sum = 0;
+pthread_mutex_t mutex;
 
 void *countPi(void *arg){
     Arg *data = (Arg *)arg;
     int iter = data->iteration;
     int id = data->thread_id;
     unsigned int seed = id;
+    long long int sum = 0;
     while(iter>0){
         double x = (double)rand_r(&seed)/RAND_MAX ;//[0,1]
         double y = (double)rand_r(&seed)/RAND_MAX ;//[0,1]
         if(x*x + y*y <= 1.0){
-            ++circle[id];
+            ++sum;
         }
         iter--;
     }
-    //std::cout<<"Thread "<<id<<" finish. circle = "<<circle[id]<<std::endl;
+
+    pthread_mutex_lock(&mutex);
+    total_sum += sum;
+    pthread_mutex_unlock(&mutex);
+
+    //std::cout<<"Thread "<<id<<" finish. circle = "<<sum<<std::endl;
     pthread_exit((void *)0);
 }
 
@@ -38,7 +46,7 @@ int main(int argc, char *argv[]){
    int thread_num = std::stoi(argv[1]);
    long long int counter = total_iter/thread_num;
 
-    Arg arg[thread_num];
+   Arg arg[thread_num];
 
    pthread_t all_threads[thread_num];
 
@@ -46,20 +54,19 @@ int main(int argc, char *argv[]){
    pthread_attr_init(&attr);
    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
+   pthread_mutex_init(&mutex, NULL);
+
    for(int i=0;i<thread_num;++i){
        arg[i].thread_id = i;
        arg[i].iteration = counter;
-       circle[i]=0;
        pthread_create(&all_threads[i],&attr,countPi,(void *)&arg[i]);
    }
    pthread_attr_destroy(&attr);
 
    void *status;
-   long long int circle_sum = 0;
    for(int i=0;i<thread_num;++i){
       pthread_join(all_threads[i],&status);
-      circle_sum += circle[i];
    }
-   std::cout<<"Final result: pi = "<<4*(long double)circle_sum/total_iter<<std::endl;
+   std::cout<<"Final result: pi = "<<4*(long double)total_sum/total_iter<<std::endl;
    pthread_exit(NULL);
 }
