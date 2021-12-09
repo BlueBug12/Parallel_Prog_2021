07bcbe5858b11ecee6f7eba8bf029d1d71f63136
BlueBug12 <benson.liu.himself@gmail.com>
2021-12-09 02:54:54 +0000

    finish kernel.cu

--- hw5/kernel.cu
+++ hw5/kernel.cu
@@ -2,11 +2,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void mandelKernel() {
+__device__ int mandel(float c_re, float c_im, int count){
+    float z_re = c_re, z_im = c_im;
+    int i;
+    for (i = 0; i < count; ++i)
+    {
+        if (z_re * z_re + z_im * z_im > 4.f)
+          break;
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+    return i;
+}
+
+__global__ void mandelKernel(float stepX, float stepY, float lowerX, float lowerY, int* output_device, int resX, int resY, int maxIterations) {
     // To avoid error caused by the floating number, use the following pseudo code
     //
     // float x = lowerX + thisX * stepX;
     // float y = lowerY + thisY * stepY;
+    int i = blockIdx.x*blockDim.x+threadIdx.x;
+    int j = blockIdx.y*blockDim.y+threadIdx.y;
+    if(i>=resX||j>=resY)
+        return;
+    float x = lowerX + i*stepX;
+    float y = lowerY + j*stepY;
+    int index = j*resX + i;
+    output_device[index] = mandel(x,y,maxIterations);
 }
 
 // Host front-end function that allocates the memory and launches the GPU kernel
@@ -14,4 +37,18 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
 {
     float stepX = (upperX - lowerX) / resX;
     float stepY = (upperY - lowerY) / resY;
+    int size = resX*resY*sizeof(int);
+    int *output_host = (int *)malloc(size);
+    int *output_device;
+    cudaMalloc(&output_device,size);
+    
+    float block_dim = 20;
+    dim3 threadsPerBlock(block_dim,block_dim);
+    dim3 numBlocks((int)ceil(resX/block_dim),(int)ceil(resY/block_dim));
+    mandelKernel<<<numBlocks,threadsPerBlock>>>(stepX,stepY,lowerX,lowerY,output_device,resX,resY,maxIterations);
+
+    cudaMemcpy(output_host,output_device,size,cudaMemcpyDeviceToHost);
+    memcpy(img,output_host,size);
+    free(output_host);
+    cudaFree(output_device);
 }
