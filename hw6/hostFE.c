49400d53e626abf9b5bace9a042abbc7e84986d9
BlueBug12 <benson.liu.himself@gmail.com>
2021-12-25 08:44:07 +0000

    pass

--- hw6/hostFE.c
+++ hw6/hostFE.c
@@ -8,6 +8,29 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             cl_context *context, cl_program *program)
 {
     cl_int status;
-    int filterSize = filterWidth * filterWidth;
+    size_t img_size = sizeof(float)*imageHeight*imageWidth;
+    size_t filter_size = sizeof(float)*filterWidth*filterWidth;
 
+    cl_command_queue cq = clCreateCommandQueue(*context,*device,0,&status);
+    
+    cl_mem c_input = clCreateBuffer(*context,CL_MEM_READ_ONLY,img_size,NULL,&status);
+    cl_mem c_output = clCreateBuffer(*context,CL_MEM_WRITE_ONLY,img_size,NULL,&status);
+    cl_mem c_filter = clCreateBuffer(*context,CL_MEM_READ_ONLY,filter_size,NULL,&status);
+    
+    status = clEnqueueWriteBuffer(cq,c_input,CL_TRUE,0,img_size,(void *)inputImage,0,NULL,NULL);
+    status = clEnqueueWriteBuffer(cq,c_filter,CL_TRUE,0,filter_size,(void *)filter,0,NULL,NULL);
+
+    //status = clBuildProgram(*program,1,*device,NULL,NULL,NULL);
+    cl_kernel c_kernel = clCreateKernel(*program,"convolution",&status);
+    clSetKernelArg(c_kernel,0,sizeof(cl_mem),(void *)&c_input);
+    clSetKernelArg(c_kernel,1,sizeof(cl_mem),(void *)&c_output);
+    clSetKernelArg(c_kernel,2,sizeof(cl_mem),(void *)&c_filter);
+    clSetKernelArg(c_kernel,3,sizeof(int),(void *)&imageWidth);
+    clSetKernelArg(c_kernel,4,sizeof(int),(void *)&imageHeight);
+    clSetKernelArg(c_kernel,5,sizeof(int),(void *)&filterWidth);
+
+    size_t globalws[2] = {imageWidth,imageHeight};
+    status = clEnqueueNDRangeKernel(cq,c_kernel,2,0,globalws,NULL,0,NULL,NULL);
+    clFinish(cq);
+    status = clEnqueueReadBuffer(cq,c_output,CL_TRUE,0,img_size,(void *)outputImage,0,NULL,NULL);
 }
--- hw6/kernel.cl
+++ hw6/kernel.cl
@@ -1,4 +1,22 @@
-__kernel void convolution() 
-{
-   
+__kernel void convolution(
+    __global float *src,
+    __global float *dest,
+    __constant float *filter,
+    int w,
+    int h,
+    int filter_size) {
+    float result = 0;
+    const int ix = get_global_id(0);
+    const int iy = get_global_id(1);
+    const int hw = filter_size >> 1;
+    for(int k = -hw;k<=hw;++k){
+        int x_idx = ix + k;
+        for(int l = -hw;l<=hw;++l){
+            int y_idx = iy + l;
+            if(x_idx>=0 && x_idx<w && y_idx>=0 && y_idx<h){
+                result += filter[(l+hw)*filter_size+(k+hw)]*src[y_idx*w+x_idx];
+            }
+        }
+    }
+    dest[iy*w+ix] = result;
 }
